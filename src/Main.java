c81f7736e88e962ed5506cac2021aef90aa1c6f4
rhoyyyalvinnn <dontmisstheblock@gmail.com>
2024-03-26 10:10:56 +0800

    oksss

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 import Arithmetic.Arithmetic;
+import Map.MyMap;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Testing Arithmetic class
+        testArithmetic();
+
+        // Testing MyMap class
+        testMyMap();
+    }
+
+    private static void testArithmetic() {
         Scanner scanner = new Scanner(System.in);
 
         // Get user input for the first number
@@ -26,6 +35,24 @@ public class Main {
         System.out.println("Maximum: " + arithmetic.getMax());
 
         scanner.close();
+    }
+
+    private static void testMyMap() {
+        // Create a MyMap instance
+        MyMap<String, Integer> myMap = new MyMap<>();
+
+        // Add key-value pairs
+        myMap.put("one", 1);
+        myMap.put("two", 2);
+        myMap.put("three", 3);
+
+        // Retrieve values
+        System.out.println("Value for key 'two': " + myMap.get("two"));
+        System.out.println("Value for key 'four': " + myMap.get("four")); // Should return null
+
+        // Remove a key-value pair
+        System.out.println("Removing key 'two': " + myMap.remove("two"));
+        System.out.println("Value for key 'two': " + myMap.get("two")); // Should return null
     }
 
     // Method to get user input and handle exceptions
